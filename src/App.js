4f56e5fe9d7961f01e7a89d019fd3673c83d80ee
Istiakahmed180 <Istiakahmed180@gmail.com>
2022-10-03 23:10:48 +0600

    practice

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Products from "./components/Products/Products";
+import Main from "./Layout/Main";
+import Friends from "./components/Friends/Friends";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        { path: "/", element: <Home></Home> },
+        { path: "/home", element: <Home></Home> },
+        { path: "/products", element: <Products></Products> },
+        {
+          path: "/friends",
+          loader: () => {
+            return fetch("https://jsonplaceholder.typicode.com/users");
+          },
+          element: <Friends></Friends>,
+        },
+      ],
+    },
+
+    { path: "/about", element: <About></About> },
+    { path: "*", element: <div>This Page Is Not Found</div> },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Friend.css";
+
+const Friend = ({ friend }) => {
+  console.log(friend);
+  return (
+    <div className="friend">
+      <h4>Name: {friend.name}</h4>
+      <p>Email: {friend.email}</p>
+      <p>
+        <small>
+          User Name: <Link to={`/friend/${friend.id}`}>{friend.username}</Link>
+        </small>
+      </p>
+    </div>
+  );
+};
+
+export default Friend;
